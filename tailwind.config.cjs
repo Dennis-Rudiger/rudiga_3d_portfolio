7b8bb1b8a8fd47dbbbbadf1333703ef76dc5ae65
Dennis-Rudiger <146859950+Dennis-Rudiger@users.noreply.github.com>
2025-01-28 13:35:52 +0300

    refactor: update Navbar component and styles, remove unused style.js file, and adjust Tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,12 +5,12 @@ module.exports = {
     theme: {
       extend: {
         colors: {
-          primary: "#050816",
-          secondary: "#aaa6c3",
-          tertiary: "#151030",
-          "black-100": "#100d25",
-          "black-200": "#090325",
-          "white-100": "#f3f3f3",
+          primary: "#111144",
+          secondary: "#223382",
+          tertiary: "#dad1c8",
+          "black-100": "#100co8",
+          "black-200": "#27221f",
+          "white-100": "#f3f4f5",
         },
         boxShadow: {
           card: "0px 35px 120px -15px #211e35",
